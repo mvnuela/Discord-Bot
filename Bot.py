5a7740223cb175d6f8545438cfeb0548732bef8a
mvnuela <marmanuelam@gmail.com>
2021-12-11 17:17:01 +0100

    saving data from ics file by $calendar command

--- Bot.py
+++ Bot.py
@@ -1,6 +1,11 @@
 import discord
+from discord import channel
 from discord.ext import commands
 from MyEvent import MyEvent
+from csv_ical import Convert
+
+# csv_ical ma niby ładnie konwertować do csva
+
 
 TOKEN="OTExMzQ3Mzk0MjU0MzQwMTI3.YZgEZg.uGCZhDpqjURs5BjapiZCxhY7awM"
 #prefixdo wywolania bota
@@ -18,7 +23,13 @@ async def addEvent(ctx,name,date,time,place):
     else:
         await ctx.send("Nie udało się dodać wydarzenia")
 @eventBot.command()
+async def calendar(ctx):
+    if ctx.message.attachments[0].filename.endswith(".ics"):
+        fil=None
+        name=ctx.message.attachments[0].filename
+        data = await ctx.message.attachments[0].read() #tutaj jest zapisany content icsa
 
+@eventBot.command()
 async def deleteEvent(ctx,name,date,time,place):
     newEvent = MyEvent(name, date, time, place)
     if (newEvent.deleteFromDB()):
